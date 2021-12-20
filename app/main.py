69f09848de7a5db956976f4c9636834749330bfe
haoweini <haoweini@gmail.com>
2021-12-20 10:23:37 -0500

    Add db.py

--- app/main.py
+++ app/main.py
@@ -4,8 +4,13 @@ from typing import Optional
 from fastapi import FastAPI
 import numpy as np
 from . import ml
+from . import config
+from . import models
+from . import db
+from cassandra.cqlengine.management import sync_table
 
 app = FastAPI()
+settings = config.get_settings()
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 
@@ -17,18 +22,19 @@ METADATA_PATH = SMS_SPAM_MODEL_DIR / "spam-classifer-metadata.json"
 
 # Model related
 AI_MODEL = None
-AI_TOKENIZER = None
-MODEL_METADATA = {}
-labels_legend_inverted = {}
+DB_SESSION = None
+SMSInference = models.SMSInference
 
 @app.on_event("startup")
 def on_startup():
-    global AI_MODEL
+    global AI_MODEL, DB_SESSION
     AI_MODEL = ml.AIModel(
         model_path = MODEL_PATH,
         tokenizer_path = TOKENIZER_PATH,
         metadata_path = METADATA_PATH
     )
+    DB_SESSION = db.get_session()
+    sync_table(SMSInference)
 
 @app.get("/")
 def read_index(q:Optional[str] = None):
